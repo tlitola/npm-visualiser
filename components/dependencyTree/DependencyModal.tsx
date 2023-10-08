898f16d2ea243ecfcfaab22ffc016ddf46e59abd
tlitola <touko@litola.net>
2023-10-08 21:06:12 +0300

    Start dependency modal

--- components/dependencyTree/DependencyModal.tsx
+++ components/dependencyTree/DependencyModal.tsx
@@ -6,11 +6,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faNpm } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
+import { PropsWithChildren } from "react";
 
 export default function DepepndencyModal({
   dependency,
   show,
   info,
+  vulns,
   hide,
 }: {
   dependency: NpmPackage;
@@ -44,16 +46,31 @@ export default function DepepndencyModal({
         </Stack>
         <CloseButton className="absolute right-6 top-6" onClick={hide} />
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className="px-4">
         <Row className="border-b-gray-200 border-b-[1px]">
-          <Col>
-            <p>Dependencies</p>
+          <Col sm={7}>
+            <ModalTitle>Description</ModalTitle>
+            <p>{info?.description}</p>
+            <ModalTitle>Dependencies</ModalTitle>
+            <p>{dependency.totalDependencies}</p>
+            <ModalTitle>Vulnerabilities</ModalTitle>
+            <div>
+              {vulns?.map((el) => (
+                <p className="break-words" key={el.name}>
+                  {el.details}
+                </p>
+              ))}
+            </div>
           </Col>
-          <Col>
-            <p>Unpacked size</p>
+          <Col sm={5}>
+            <p>Weekly downloads</p>
           </Col>
         </Row>
       </Modal.Body>
     </Modal>
   );
+}
+
+function ModalTitle({ children }: PropsWithChildren) {
+  return <h5>{children}</h5>;
 }
