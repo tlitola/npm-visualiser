1e0874dae260933cf44c262e9c349bc7464dac66
tlitola <touko@litola.net>
2023-11-07 15:57:30 +0200

    Fix report page styling

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -2,7 +2,7 @@ import DependencyTreePage from "@/components/dependencyTree/DependencyTreePage";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col px-4 py-16 items-center bg-gray-50 ">
+    <main className="flex h-screen flex-col px-4 py-16 items-center bg-gray-50 ">
       <DependencyTreePage />
     </main>
   );
