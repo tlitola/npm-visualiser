d10f0e0bef8e30fc2d94371d7ee62a58cab85f19
tlitola <touko@litola.net>
2024-02-29 00:03:59 +0200

    Remove unused file

--- utils/protocol.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { NpmPackage } from "./PackageLock";
-import { DepGraph } from "dependency-graph";
-
-export const loadingStatusUpdate = z.tuple([z.literal("loadingStatus"), z.number(), z.number(), z.string()]);
-export type LoadingStatusUpdate = z.infer<typeof loadingStatusUpdate>;
-
-export type ParseCompleteMessage = ["complete", DepGraph<NpmPackage>];
